14763919574f7d3e96265a0bb669b7833d503a67
Maynard Louis E. Prepotente <maynard.prepotente@gmail.com>
2016-05-17 22:01:21 -0700

    Update memory_check.sh

--- memory_check.sh
+++ memory_check.sh
@@ -5,3 +5,25 @@
 free
 TOTAL_MEMORY=$( free | grep Mem: | awk '{ print ( $3 / $2 ) * 100 ')
 echo "Memory Usage: " $TOTAL_MEMORY"%"
+
+if [ $# -eq 0]
+  then 
+    echo "Please Supply Required Parameters:"
+    echo "-c : critical threshold"
+    echo "-w : warning threshold"
+    echo "-e : email to send the report"
+fi
+
+while getopts "c:w:e:" opt; do
+
+  case $opt in
+  
+  c ) echo "Critical Threshold: " $OPTARG;;
+  w ) echo "Warning Threshold: " $OPTARG;;
+  e ) echo "Email: " $OPTARG;;
+  \?) echo "Invalid Argument"ll
+  
+  esac
+done
+
+shift $(($OPTIND -1))
